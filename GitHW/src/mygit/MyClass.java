2b5841db6981ff6cca53623eb68784823fd78ded
catomat0 <catomat0@github.com>
2025-12-03 14:35:32 +0900

    Merge

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,13 +5,9 @@ public class MyClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git!");
-<<<<<<< HEAD
 		System.out.println("Changing in Master Branch!");
-
-=======
 		System.out.println("Master Branch!");
 		YourClass yourClass = new YourClass();
 		yourClass.sayHello("Everyone");
->>>>>>> refs/heads/dev
 	}
 }
